3142027210078624a81e833b1f27534e70d2f791
Kathleen B <kathleen.bongco@gmail.com>
2026-03-09 12:16:36 -0400

    visual testing and card component completed

--- app/page.tsx
+++ app/page.tsx
@@ -19,8 +19,15 @@ export default function Home() {
       <Badge badgeType={"neutral"}>Upcoming</Badge>
       <div style={{ marginBottom: "16px" }}>
         <Card>
-          <CardHeader title="Weigh-Ins & Equipment Check"></CardHeader>
-          <CardBody>All Athletes Duration: 2hrs Room: Hall B</CardBody>
+          <CardHeader title="Weigh-Ins & Equipment Check"
+            extraButton={<Badge badgeType={"completed"}>Done</Badge>}
+          ></CardHeader>
+          <CardBody>
+            <span style={{ display: 'flex', gap: '0.5rem' }}>
+          <p style={{marginBottom: 0, marginTop: 0}}>All Athletes</p>
+              <p style={{ color: '#4a6070', marginBottom: 0, marginTop: 0 }}>Duration:</p><p style={{ marginBottom: 0, marginTop: 0 }}>2hrs</p>
+               <p style={{ color: '#4a6070', marginBottom: 0, marginTop: 0  }}>Room:</p><p style={{marginBottom: 0, marginTop: 0}}>Hall B</p>
+            </span></CardBody>
         </Card>
       </div>
       <div style={{ marginTop: "16px" }}>
@@ -30,11 +37,13 @@ export default function Home() {
             extraButton={<Badge badgeType={"success"}>Live Now</Badge>}
           ></CardHeader>
           <CardBody>
-            <span>
-              <p style={{ color: '#4a6070', paddingBottom: 0 }}>Lifters:</p><p>22</p>
+            <span style={{display: 'flex', gap:'0.5rem'}}>
+              <p style={{ color: '#4a6070', marginBottom: 0, marginTop: 0 }}>Lifters:</p><p style={{marginBottom: 0, marginTop: 0}}>22</p>
+              <p style={{ color: '#4a6070', marginBottom: 0, marginTop: 0  }}>Platform:</p><p style={{marginBottom: 0, marginTop: 0}}>2</p>
+              <p style={{ color: '#4a6070', marginBottom: 0, marginTop: 0 }}>Progress:</p><p style={{ color: '#00e676', marginBottom: 0, marginTop: 0 }}>62% complete</p>
+              <p style={{ color: '#4a6070', marginBottom: 0, marginTop: 0 }}>Est. End:</p><p style={{marginBottom: 0, marginTop: 0}}>~5:45 PM</p>
             </span>
-            {/* Lifters: 22, Platforms: 2, Progress: 62% Complete, Est End: 5:45PM */}
-            <div style={{ paddingTop: "0.5rem" }}>
+            <div style={{ paddingTop: "0.5rem", display: 'flex', gap: '0.5rem' }}>
               <Badge badgeType={"success"}>Platform 1-Flight A Sq R3</Badge>
               <Badge badgeType={"completed"}>Platform 2-Flight B Bp R2</Badge>
             </div>
