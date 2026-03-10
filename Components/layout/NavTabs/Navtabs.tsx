20894d7ebf85167e827e7edac42debae4f94db3c
Kathleen B <kathleen.bongco@gmail.com>
2026-03-10 13:13:16 -0400

    added navtabs

--- Components/layout/NavTabs/Navtabs.tsx
+++ Components/layout/NavTabs/Navtabs.tsx
@@ -4,10 +4,10 @@ export default function NavTabs() {
   return (
     <div className='chbi-nav-tab-container'>
       <ul className='chbi-nav-tab-list'>
-        <li className='chbi-nav-tab-item'>Scoreboard</li>
-        <li className='chbi-nav-tab-item'>Results</li>
-        <li className='chbi-nav-tab-item'>Schedule</li>
-        <li className='chbi-nav-tab-item'>Admin</li>
+<li className='chbi-nav-tab-item chbi-nav-tab-item--active'>Scoreboard</li>
+<li className='chbi-nav-tab-item'>Results</li>
+<li className='chbi-nav-tab-item'>Schedule</li>
+<li className='chbi-nav-tab-item'>Admin</li>
       </ul>
     </div>
   )
