a45f79dbcdc2dac5c42bd3e2e4c975618c1a5c42
Kathleen B <kathleen.bongco@gmail.com>
2026-03-25 09:50:26 -0400

    starting skeleton for schedule view

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ import AppShell from "@/Components/AppShell/AppShell";
 import { useState } from 'react';
 import { Tab } from "@/Components/layout/layout-props";
 import MeetView from "./meets/page";
+import ScheduleView from "./schedule/page";
 export default function Home() {
 
   const [activeTab, setActiveTab] = useState<Tab>('scoreboard');
@@ -15,7 +16,11 @@ export default function Home() {
         {/* <div style={{ padding: '20px', color: 'white' }}>
           current tab: {activeTab}
         </div> */}
-                  <MeetView/>
+        {/* <MeetView/> */}
+          {activeTab === 'scoreboard' && <MeetView />}
+  {/* {activeTab === 'results' && <ResultsView />} */}
+  {activeTab === 'schedule' && <ScheduleView />}
+  {/* {activeTab === 'admin' && <AdminView />} */}
       </AppShell>
     </>
   );
---
+++ app/schedule/page.tsx
@@ -0,0 +1,7 @@
+export default function ScheduleView() {
+  return (
+    <>
+      <h1>This is the schedule view</h1>
+    </>
+)
+}
