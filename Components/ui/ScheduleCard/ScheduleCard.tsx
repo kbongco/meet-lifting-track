284ba48c1bb35f1003dce7a54f3972e9722d4d82
Kathleen B <kathleen.bongco@gmail.com>
2026-03-31 15:09:10 -0400

    started working on scheduling cards

--- Components/ui/ScheduleCard/ScheduleCard.tsx
+++ Components/ui/ScheduleCard/ScheduleCard.tsx
@@ -1,7 +1,16 @@
+import ScheduleProgress from '../ScheduleProgress/ScheduleProgress';
+import './ScheduleCard.scss';
+
 export default function ScheduleCards() {
   return (
-    <>
-      <h1>Schedule Card</h1>
-    </>
+    <div className='chbi-lifting-schedule-card-container'>
+      <h1 className='chbi-lifting-schedule-header '>Session 1</h1>
+      <div className='chbi-lifting-schedule-info'>
+        <p className='chbi-lifting-info'>Sun, Feb 23 · 8:00 AM</p>
+        <p className='chbi-lifting-info'>F63 · M74 · Open Raw</p>
+        <p className='chbi-lifting-info'>28 lifters · 2 platforms</p>
+      </div>
+      <ScheduleProgress/>
+    </div>
   )
 }
---
+++ Components/ui/ScheduleProgress/ScheduleProgress.tsx
@@ -0,0 +1,11 @@
+import './ScheduleProgress.scss'
+
+export default function ScheduleProgress() {
+  return (
+    <div className='chbi-lifting-progress-container'>
+      <div className='chbi-lifting-progress-fill'>
+        <span className='chbi-lifting-progress-span'></span>
+      </div>
+    </div>
+  )
+}
