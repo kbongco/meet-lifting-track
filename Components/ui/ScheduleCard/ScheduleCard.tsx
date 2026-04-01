dd4fa88c9a921e000c1ffa978bd79d2d76befef5
Kathleen B <kathleen.bongco@gmail.com>
2026-04-01 11:54:55 -0400

    completed schedule card

--- Components/ui/ScheduleCard/ScheduleCard.tsx
+++ Components/ui/ScheduleCard/ScheduleCard.tsx
@@ -1,10 +1,14 @@
 import ScheduleProgress from '../ScheduleProgress/ScheduleProgress';
+import ScheduleStatus from '../ScheduleStatus/ScheduleStatus';
 import './ScheduleCard.scss';
 
 export default function ScheduleCards() {
   return (
     <div className='chbi-lifting-schedule-card-container'>
+      <div className='chbi-lifting-schedule-header-container'>
       <h1 className='chbi-lifting-schedule-header '>Session 1</h1>
+        <ScheduleStatus />
+        </div>
       <div className='chbi-lifting-schedule-info'>
         <p className='chbi-lifting-info'>Sun, Feb 23 · 8:00 AM</p>
         <p className='chbi-lifting-info'>F63 · M74 · Open Raw</p>
---
+++ Components/ui/ScheduleStatus/ScheduleStatus.tsx
@@ -0,0 +1,9 @@
+import './ScheduleStatus.scss';
+
+export default function ScheduleStatus() {
+  return (
+    <div className='chbi-lifting-schedule-status'>
+     <span className='chbi-lifting-schedule-text'>done</span>
+    </div>
+  )
+}
