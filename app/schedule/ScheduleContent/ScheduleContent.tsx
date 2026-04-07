f364c7bdcf9738d0ebba495b57e144df233585b6
Kathleen B <kathleen.bongco@gmail.com>
2026-04-07 11:13:17 -0400

    starting the schedule content

--- app/schedule/ScheduleContent/ScheduleContent.tsx
+++ app/schedule/ScheduleContent/ScheduleContent.tsx
@@ -1,7 +1,8 @@
 export default function ScheduleContent() {
   return (
     <>
-      <h1>This is the schedule content</h1>
+      <h1>Sunday February 23 2026</h1>
+      <p>Day 1 of 2 · 3 sessions · 2026 USAPL Nationals</p>
     </>
   )
 }
