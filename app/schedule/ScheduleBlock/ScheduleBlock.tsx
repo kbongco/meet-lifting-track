17cccd49dce87b57e9711bd1644a85eb8f431fb1
Kathleen B <kathleen.bongco@gmail.com>
2026-04-09 11:21:35 -0400

    started adding styules in for schedule block

---
+++ app/schedule/ScheduleBlock/ScheduleBlock.tsx
@@ -0,0 +1,15 @@
+import './ScheduleBlock.scss';
+
+export default function ScheduleBlock() {
+  return (
+    <>
+      <div className='chbi-lifting-circle-time'>
+        <p>7:00Am</p>
+        <div className='chbi-lifting-circle-div'>
+      <span className='chbi-lifting-circle-status'></span>
+      </div>
+        </div>
+      <h1>This is the schedule Block</h1>
+    </>
+  )
+}
--- app/schedule/ScheduleContent/ScheduleContent.tsx
+++ app/schedule/ScheduleContent/ScheduleContent.tsx
@@ -1,8 +1,11 @@
+import ScheduleBlock from "../ScheduleBlock/ScheduleBlock";
+
 export default function ScheduleContent() {
   return (
     <>
       <h1>Sunday February 23 2026</h1>
       <p>Day 1 of 2 · 3 sessions · 2026 USAPL Nationals</p>
+      <ScheduleBlock/>
     </>
   )
 }
